515cdbbe722d386f3d86201a7df712d2aebebabc
xijieyin <xijieyin@gmail.com>
2022-05-26 18:30:43 +0800

    fix: update api base url

--- example/src/helpers/api.ts
+++ example/src/helpers/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios'
 import { IAssetData, IGasPrices, IParsedTx } from './types'
 
 const api: AxiosInstance = axios.create({
-  baseURL: 'https://ethereum-api.xyz',
+  baseURL: 'https://shrouded-lowlands-66998.herokuapp.com/',
   timeout: 30000, // 30 secs
   headers: {
     Accept: 'application/json',
